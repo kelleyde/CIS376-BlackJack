34d2abc30457f83869fc274a679581eb1e239269
Delaney Kelley <179527594+kelleyde@users.noreply.github.com>
2026-01-21 08:59:23 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "deck.h"
+#include <iostream>
+
+int main(int argc, char** argv){
+	// Create a deck
+	Deck deck;
+	// Shuffle the deck (notice the static-method call)
+	Deck::shuffle(deck);
+	// Auto keyword deduces type for us, so we don't have to.  Here
+	// it is for creating an iterator.
+	for(auto it = deck.begin(); it != deck.end(); ++it){
+		std::cout << *it << "\t";
+	}
+	std::cout << std::endl;
+}
