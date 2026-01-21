85e8ba6d813e233ce72f13a97513a7fb32462b13
Delaney Kelley <kelleyde@mail.gvsu.edu>
2026-01-21 13:36:37 -0500

    added user input loop and ability to quit the loop.

--- main.cpp
+++ main.cpp
@@ -12,4 +12,24 @@ int main(int argc, char** argv){
 		std::cout << *it << "\t";
 	}
 	std::cout << std::endl;
+	
+	std::string user_input;
+	int i = 1;
+	// Game play loop. If player types quit loop exits.
+	do {
+		std::cin >> user_input;
+		std::cout << "Your input: " << user_input << std::endl;
+		// convert user_input to lowercase
+		// code from 
+		// https://stackoverflow.com/questions/26202113/how-to-make-case-sensitivity-not-matter-when-inputting-an-answer 
+		for (auto i = user_input.begin(); i < user_input.end(); i ++) {
+			*i = tolower(*i);
+		}
+
+		std::cout << "Your lower finput: " << user_input << std::endl;
+		if (user_input == "quit") {
+		i = i-1;
+		}
+	} while (i != 0);
+
 }
