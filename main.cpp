63f8aa4050d5a262bde746b05d2a093df719548c
Delaney Kelley <kelleyde@mail.gvsu.edu>
2026-01-28 09:05:32 -0500

    deck[], getNumber()

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "deck.h"
 #include <iostream>
 
+/Card& deal(int num_cards_dealt){}
+
 int main(int argc, char** argv){
 	// Create a deck
 	Deck deck;
@@ -8,65 +10,73 @@ int main(int argc, char** argv){
 	Deck::shuffle(deck);
 	// Auto keyword deduces type for us, so we don't have to.  Here
 	// it is for creating an iterator.
-	for(auto it = deck.begin(); it != deck.end(); ++it){
-		std::cout << *it << "\t";
-	}
-	std::cout << std::endl;
+	// for(auto it = deck.begin(); it != deck.end(); ++it){
+	// 	std::cout << *it << "\t";
+	// }
+	// std::cout << std::endl;
 	
 	std::string user_input;
+	int betMoney;
 	int i = 1; // indicates if game is running
 	int money = 100;
 
-	std::cin >> user_input;
-		std::cout << "How much would you like to bet: " << user_input << std::endl;
-		// check to make sure input is an integer, non-negative, and not greater than the current money they have
-		// store their input as betMoney
+	Card& first_deal = deck[0];
+	std::cout << first_deal << std::endl;
 
-	// dealerPoints = 0
-	// playerPoints = 0
+	int card_num = 
 
-	// Game play loop. If player types quit loop exits.
-	// deal 2 cards to dealer, deal 2 cards to player
-	// Thinking calling Deck::shuffle(deck) at index 0, but incrementing the index each time the "pull card" function is called
-	do {
-		// check if playerPoints > 21, if so they lose and int money -= betMoney
-		// check if money == 0, if so, force quit game 
-		// check if playerPoints == 21, if so they win and int money += betMoney
 
-		std::cin >> user_input;
-		std::cout << "Your input: " << user_input << std::endl;
-		// convert user_input to lowercase
-		// code from 
-		// https://stackoverflow.com/questions/26202113/how-to-make-case-sensitivity-not-matter-when-inputting-an-answer 
-		for (auto i = user_input.begin(); i < user_input.end(); i ++) {
-			*i = tolower(*i);
-		}
+// 	std::cout << "How much would you like to bet: ";
+// 	std::cin >> betMoney;
+// 	std::cout << "You bet "<< betMoney <<"$" << std::endl;
+// 	// check to make sure input is an integer, non-negative, and not greater than the current money they have
+// 	// store their input as betMoney
 
-		if (user_input == "quit") {
-		i = i-1;
-		}
+// 	// dealerPoints = 0
+// 	// playerPoints = 0
+
+// 	// Game play loop. If player types quit loop exits.
+// 	// deal 2 cards to dealer, deal 2 cards to player
+// 	// Thinking calling Deck::shuffle(deck) at index 0, but incrementing the index each time the "pull card" function is called
+// 	do {
+// 		// check if playerPoints > 21, if so they lose and int money -= betMoney
+// 		// check if money == 0, if so, force quit game 
+// 		// check if playerPoints == 21, if so they win and int money += betMoney
+
+// 		std::cin >> user_input;
+// 		std::cout << "Your input: " << user_input << std::endl;
+// 		// convert user_input to lowercase
+// 		// code from 
+// 		// https://stackoverflow.com/questions/26202113/how-to-make-case-sensitivity-not-matter-when-inputting-an-answer 
+// 		for (auto i = user_input.begin(); i < user_input.end(); i ++) {
+// 			*i = tolower(*i);
+// 		}
+
+// 		if (user_input == "quit") {
+// 		i = i-1;
+// 		}
 		
-		// take another card
-		if (user_input == "hit") {
-			// print the card at the top of the Shuffled Deck stack and store the points via card.number
-		}
+// 		// take another card
+// 		if (user_input == "hit") {
+// 			// print the card at the top of the Shuffled Deck stack and store the points via card.number
+// 		}
 
-		// double bet, take one more card
-		if (user_input == "double down") {
-			// return 2 cards at the top of the Shuffled Deck stack and store the points
-		}
+// 		// double bet, take one more card
+// 		if (user_input == "double down") {
+// 			// return 2 cards at the top of the Shuffled Deck stack and store the points
+// 		}
 		
-		// keep your hand
-		if (user_input == "stand") {
-			//reveal dealer's points, compare who is closer to 21
+// 		// keep your hand
+// 		if (user_input == "stand") {
+// 			//reveal dealer's points, compare who is closer to 21
 
-			// Dealer's turn
-				// reveal "hole card"
-				// if dealerPoints < 17, make them hit
-				// if dealerPoints < playerPoints, make them hit
-			// Compare playerPoints and dealerPoints, if playerPoints < dealerPoints, player loses. if playerPoints > dealerPoints, player wins. if equal, nothing happens
-		}
+// 			// Dealer's turn
+// 				// reveal "hole card"
+// 				// if dealerPoints < 17, make them hit
+// 				// if dealerPoints < playerPoints, make them hit
+// 			// Compare playerPoints and dealerPoints, if playerPoints < dealerPoints, player loses. if playerPoints > dealerPoints, player wins. if equal, nothing happens
+// 		}
 
-	} while (i != 0);
+// 	} while (i != 0);
 
-}
+ }
